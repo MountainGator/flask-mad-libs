535ed958bcf13af585609dd7118e3c9ab2d51c17
LukeSandersJet <lsanders@jet-airways.com>
2022-04-15 15:04:54 -0500

    solve exercise I think

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 from flask import Flask
+from stories import story
 
 app = Flask(__name__)
+
+@app.route('/')
+def questions():
+    words = story.prompts
+
+    return render_template('index.html', words= words)
+
+@app.route('/story')
+def add_story():
+    text = story.generate(request.args)
+
+    return render_template('story.html', text= text)
